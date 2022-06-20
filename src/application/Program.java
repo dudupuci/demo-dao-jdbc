b960333f0d34218fd5ec02cf21109592e9951638
dudupuci <dudupucinelli@gmail.com>
2022-06-20 15:52:29 -0300

    Seller entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.text.ParseException;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ParseException {
+
 		Department obj = new Department(1, "Books");
-		
+		Seller seller = new Seller(2, "Bob Grey", "bob@gmail.com", new Date(), 2500.50, obj);
+
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
