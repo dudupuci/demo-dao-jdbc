d4e837778b0c6fef62a97397dcfcb238da3fdb26
dudupuci <dudupucinelli@gmail.com>
2022-06-20 16:21:16 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,20 +2,23 @@ package application;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.Locale;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
 
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(2, "Bob Grey", "bob@gmail.com", new Date(), 2500.50, obj);
 
-		System.out.println(obj);
-		System.out.println(seller);
-
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 	}
 
 }
